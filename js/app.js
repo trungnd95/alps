7f2900cbfaa73e8fe2bdbc4b9d3578442233293d
TrungN <trungnd233@viettel.com.vn>
2019-10-03 13:13:29 +0700

    Finished desktop version

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log(9);
+window.addEventListener("DOMContentLoaded", (event) => {
+  const navbarBullet = document.querySelector(".app-navbar-bullet");
+  navbarBullet.addEventListener("click", function() {
+    const navbar = document.querySelector(".app-navbar");
+    navbar.classList.toggle("visible");
+  })
+})
