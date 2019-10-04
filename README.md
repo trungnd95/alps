b7fd2ba1f1ead9b18063743d662e3418f727b710
TrungN <trungnd233@viettel.com.vn>
2019-10-04 11:35:06 +0700

    App already reponsived. Done

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# ALPSLAPINE project
-
-##### Trung Ng
+# Alps project
 
 ---
+
+### 1. Short description
+
+    Project from Cinamon that require translate psd to html. Project supports to view on multiple devices (reponsive devices).
+
+### 2. Techs List
+
+| **Grid Sytem**         | Boostrap 4, Flexbox |
+| ---------------------- | ------------------- |
+| **Source CSS Files**   | SCSS                |
+| **Font**               | Avenir, Cowell      |
+| **Media Queries Unit** | px                  |
+| **Global Font Size**   | 16px == 1rem        |
+| **Primary CSS Unit**   | rem                 |
+| **Build tool**         | Webpack             |
+
+### 3. Run
+
+- Clone project
+- Go to project folder, open terminal to install package: `npm i`
+- Run **live-server**: `npm start`
+
+---
+
+@Trung Ng
--- js/app.js
+++ js/app.js
@@ -6,10 +6,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
     navbar.classList.toggle("visible");
   })
 
+  const appnavBullet = document.querySelector(".app-nav-bullet");
+  appnavBullet.addEventListener("click", function() {
+    const navs = document.querySelectorAll("nav");
+    navs.forEach(function(nav) {
+      nav.classList.toggle("visible");
+    })
+  })
+
   //---- Left sidebar toggle
-  const sidebarBullet =  document.querySelector(".sidebar-toggle-bullet");
-  sidebarBullet.addEventListener("click", (e) => {
+  function toggleSidebar() {
     const sidebar = document.querySelector(".left-sidebar");
     sidebar.classList.toggle("sidebar-visible");
+  }
+  const sidebarBullet =  document.querySelector(".sidebar-toggle-bullet");
+  sidebarBullet.addEventListener("click", (e) => {
+    toggleSidebar();
+  })
+
+  const sidebarBulletXs =  document.querySelector(".sidebar-toggle-bullet-xs");
+  sidebarBulletXs.addEventListener("click", (e) => {
+    toggleSidebar();
   })
 })
