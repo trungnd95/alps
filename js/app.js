2d4b808449d968796b980c9e5f5075dcea0958ea
Trung Ng <trungnd.uet@gmail.com>
2019-10-04 07:15:23 +0700

    Completed responsive tablet device, mobile is on progress

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  //---- Header navbar toggle
   const navbarBullet = document.querySelector(".app-navbar-bullet");
   navbarBullet.addEventListener("click", function() {
     const navbar = document.querySelector(".app-navbar");
     navbar.classList.toggle("visible");
+  })
+
+  //---- Left sidebar toggle
+  const sidebarBullet =  document.querySelector(".sidebar-toggle-bullet");
+  sidebarBullet.addEventListener("click", (e) => {
+    const sidebar = document.querySelector(".left-sidebar");
+    sidebar.classList.toggle("sidebar-visible");
   })
 })
